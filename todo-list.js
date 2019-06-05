37a0a5a818a1d0eb1d77eae5ecef2a6e274463d4
yaleksandrova <yaleksandrova@yahoo.com>
2019-06-04 20:48:36 -0600

    Add DFS fn

--- todo-list.js
+++ todo-list.js
@@ -10,4 +10,20 @@ class Task {
     localStorage.setItem("todos", JSON.stringify(todoTasks));
   }
 
+  deleteFromStorage(index) {
+    todoTasks.splice(index, 1);
+    this.saveToStorage();
+  }
+
+  updateToDo() {
+    this.urgency = !this.urgency; 
+    this.saveToStorage();
+  }
+
+  updateTask(index) {
+    this.taskList[index].done = !this.taskList[index].done;
+    this.saveToStorage();
+  }
+}
+
 
